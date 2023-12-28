bb7bd753b9ff009b281da6e13b1a0df0f5b99a8b
a2nfinance <levi@a2n.finance>
2023-12-28 17:34:53 +0700

    add .env.example

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-    reactStrictMode: true,
-    env: {
-        dbUrl: process.env.dburl,
-        contractAddress: process.env.contractAddress
-    },
+    reactStrictMode: false,
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
